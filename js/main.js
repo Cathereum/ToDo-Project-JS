372cf2e4bbe4b75254de7f0664438e9d1b2dd98c
Cathereum <106606021+Cathereum@users.noreply.github.com>
2022-10-29 23:15:16 +0700

    Add arrays for tasks

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ const taskInput = document.querySelector("#taskInput");
 const tasksList = document.querySelector("#tasksList");
 const emptyList = document.querySelector("#emptyList");
 
+let allTasks = [];
+
 form.addEventListener("submit", addTask);
 tasksList.addEventListener("click", deleteTask);
 tasksList.addEventListener("click", completeTask);
@@ -11,8 +13,18 @@ function addTask(event) {
   event.preventDefault();
   const taskText = taskInput.value;
 
-  const taskHTML = `<li class="list-group-item d-flex justify-content-between task-item">
-  <span class="task-title">${taskText}</span>
+  const newTask = {
+    id: Date.now(),
+    text: taskText,
+    done: false,
+  };
+
+  allTasks.push(newTask);
+
+  const cssClass = newTask.done ? "task-title task-title--done" : "task-title";
+
+  const taskHTML = `<li id="${newTask.id}" class="list-group-item d-flex justify-content-between task-item">
+  <span class="${cssClass}">${newTask.text}</span>
   <div class="task-item__buttons">
       <button type="button" data-action="done" class="btn-action">
           <img src="./img/tick.svg" alt="Done" width="18" height="18">
@@ -37,6 +49,11 @@ function deleteTask(event) {
   if (event.target.dataset.action !== "delete") return;
 
   const removedTask = event.target.closest(".list-group-item");
+
+  const id = Number(removedTask.id);
+
+  allTasks = allTasks.filter((task) => task.id !== id);
+
   removedTask.remove();
 
   if (tasksList.children.length === 1) {
@@ -48,6 +65,15 @@ function completeTask(event) {
   if (event.target.dataset.action !== "done") return;
 
   const completedTask = event.target.closest(".list-group-item");
+
+  const id = Number(completedTask.id);
+
+  const task = allTasks.find((task) => task.id === id);
+
+  task.done = !task.done;
+
+  console.log(task);
+
   const taskTitle = completedTask.querySelector(".task-title");
   taskTitle.classList.toggle("task-title--done");
 }
